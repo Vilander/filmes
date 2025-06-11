44b8ab6b7013ed49b2abe7c2802e547c0d75d15e
Vilander Costa <vilander.costa@gmail.com>
2025-06-10 22:15:42 -0300

    pegando dados json

--- app.js
+++ app.js
@@ -6,28 +6,28 @@ let filme1 = {
     titulo: "Jurassic Park",
     foto: "filme1.webp",
     avaliacao: "10/10",
-    duracao: "1h50m"
+    duracao: "2h07m"
 }
 
 let filme2 = {
     titulo: "Karate Kid - Lendas",
     foto: "filme2.jpg",
     avaliacao: "8/10",
-    duracao: "1h45m"
+    duracao: "1h34m"
 }
 
 let filme3 = {
     titulo: "Lilo & Stitch",
     foto: "filme3.jpg",
     avaliacao: "10/10",
-    duracao: "1h10m"
+    duracao: "1h48m"
 }
 
 let filme4 = {
     titulo: "Pecadores",
     foto: "filme4.jpg",
     avaliacao: "9/10",
-    duracao: "2h05m"
+    duracao: "2h17m"
 }
 let filme5 = {
     titulo: "Um Filme Minecraft",
@@ -35,21 +35,35 @@ let filme5 = {
     avaliacao: "8/10",
     duracao: "1h41m"
 }
+let filme6 = {
+    titulo: "Batman: O Cavaleiro das Trevas",
+    foto: "filme6.jpg",
+    avaliacao: "10/10",
+    duracao: "2h32m"
+}
 
-function fnMontarCartao(filmeAtual) {
-    document.querySelector(".lista-filmes").innerHTML +=`
+// function fnMontarCartao(filmeAtual) {
+//     document.querySelector(".lista-filmes").innerHTML +=`
+//         <div class="card-filme">
+//             <img src="img/${filmeAtual.foto}">
+//             <h3>${filmeAtual.titulo}</h3>
+//             <span>⭐ ${filmeAtual.avaliacao}</span>
+//         </div>
+//         `
+// }
+
+//DOM - Document Object Model =>Estrutura do HTML
+
+// fnMontarCartao(filme1)
+
+let todosOsFilmes = [filme1, filme2, filme3, filme4, filme5]
+
+todosOsFilmes.forEach((filmeAtual) => {
+    document.querySelector(".lista-filmes").innerHTML += `
         <div class="card-filme">
             <img src="img/${filmeAtual.foto}">
             <h3>${filmeAtual.titulo}</h3>
             <span>⭐ ${filmeAtual.avaliacao}</span>
         </div>
         `
-}
-
-//DOM - Document Object Model =>Estrutura do HTML
-
-fnMontarCartao(filme1)
-fnMontarCartao(filme2)
-fnMontarCartao(filme3)
-fnMontarCartao(filme4)
-fnMontarCartao(filme5)
+})
